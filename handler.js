40b5b9f8e5803649ed113f056a624e937a9e0717
Pawel Niewiadomski <11110000b@gmail.com>
2017-03-02 22:20:14 +0100

    added first version that triggers wercker build

--- handler.js
+++ handler.js
@@ -1,17 +1,44 @@
 'use strict';
 
-module.exports.publishPawelNiewiadomskiCom = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-      env: process.env
-    }),
+module.exports.triggerBuild = (event, context, callback) => {
+  const request = require('request');
+
+  const payload = {
+    'pipelineId': process.env.applicationId,
+    'message': "Automatically triggered"
   };
 
-  callback(null, response);
+  const req = request.post({
+    url: 'https://app.wercker.com/api/v3/runs/',
+    body: payload,
+    json: true,
+    headers: {
+      'Authorization': 'Bearer ' + process.env.apiKey
+    }
+  }, (error, response, body) => {
+    callback(null, body);
+  });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.fetchApplications = (event, context, callback) => {
+const request = require('request');
+  const req = request.get({
+    url: 'https://app.wercker.com/api/v3/applications/' + process.env.username,
+    json: true,
+    headers: {
+      'Authorization': 'Bearer ' + process.env.apiKey
+    }
+  }, (error, response, body) => {
+    const apps = body.map((app) => {
+      return {
+        id: app.id,
+        name: app.name,
+        stack: app.stack
+      }
+    });
+    callback(null, apps);
+  });
+}
